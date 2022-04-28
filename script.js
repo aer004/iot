f42ddb418fc73a302424003d7e4708843f01b505
aer004 <aer004@example.com>
2022-04-28 17:36:20 -0400

    Added JSON DOM thingy in comment

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /* PROF NOTE: Updated to be an array of all dragItem class objects */
-// var dragItem = document.querySelector("#item");
-var dragItems = document.getElementsByClassName("draggableItem");
-var container = document.querySelector("#container");
+var dragItems;
+var container;
 
 /* PROF NOTE: Now that we have multiple draggable items we need a way to
               store their modified position properties. We can use ann array of JSON */
@@ -19,39 +18,125 @@ var yOffset = 0;
               so we need a way to check which our initially clicked element was */
 var initialDragElem;
 
-/* PROF NOTE: Your initial code was listening for mouse events on the container,
-              rather than on the items themselves */
-
-// container.addEventListener("touchstart", dragStart, false);
-// container.addEventListener("touchend", dragEnd, false);
-// container.addEventListener("touchmove", drag, false);
-//
-// container.addEventListener("mousedown", dragStart, false);
-// container.addEventListener("mouseup", dragEnd, false);
-// container.addEventListener("mousemove", drag, false);
-
-window.addEventListener("click", print);
-
-function print(){
-  console.log("HELLLLLLLLLLLO)")
+var macTemplate = {
+  "class": "draggableItem",
+  "id": "item",
+  "header": {
+    "class": "draggableItemHeader",
+    "id": "itemHeader",
+    "text": ".mac",
+    "padding": "10px",
+    "cursor": "move",
+    "z-index": "10",
+    "background-color": "#FFFFFF",
+    "border": "#000000",
+    "border-style": "none none double none",
+    "border-width": "none none medium none",
+    "color": "#000000"
+  },
+  "img": "mac.jpg",
+  "footer": {
+    "class": "draggableItemFooter",
+    "id": "itemFooter",
+    "border": "#000000",
+    "background-color": "#FFFFFF",
+    "z-index": "10",
+    "padding": "10px",
+    "border-style": "solid none none none",
+    "border-top-width": "medium",
+    "color": "#000000"
+  },
+  "touch-action": "none",
+  "user-select": "none",
+  "margin": "20px",
+  "background-color": "#FFFFFF",
+  "border": "solid #000000",
+  "border-width": "medium thick thick medium",
+  "text-align": "center",
+  "clip-path": "polygon(0 0, 0 0, 98% 0, 100% 2%, 100% 100%, 100% 100%, 2% 100%, 0% 98%, 0% 0%)"
 }
 
-for (item of dragItems) {
-  item.addEventListener("touchstart", dragStart, false);
-  item.addEventListener("touchend", dragEnd, false);
-  item.addEventListener("touchmove", drag, false);
+document.addEventListener("DOMContentLoaded", function(){
+  dragItems = document.getElementsByClassName("draggableItem");
+  container = document.querySelector("#container");
+  window.addEventListener("click", print);
 
-  item.addEventListener("mousedown", dragStart, false);
-  item.addEventListener("mouseup", dragEnd, false);
-  item.addEventListener("mousemove", drag, false);
+  /*Adds addEventListeners to all items with class "draggableItem"*/
+  for (item of dragItems) {
+    item.addEventListener("touchstart", dragStart, false); //calls dragStart
+    item.addEventListener("touchend", dragEnd, false); //calls dragEnd
+    item.addEventListener("touchmove", drag, false); //calls drag
 
-  /* Adding an entry to our draggableData storage */
-  var newDraggableData = {
-    "elementReference": item,
-    "offset_x": 0,
-    "offset_y": 0,
+    item.addEventListener("mousedown", dragStart, false);
+    item.addEventListener("mouseup", dragEnd, false);
+    item.addEventListener("mousemove", drag, false);
+
+    /* Adding an entry to our draggableData storage */
+    var newDraggableData = {
+      "elementReference": item,
+      "offset_x": 0,
+      "offset_y": 0,
+    };
+    draggableData.push(newDraggableData);
   };
-  draggableData.push(newDraggableData);
+
+});
+/*
+function addMacElements(template, num){
+  for (let i = 1; i =< num; i++) {
+    let newMacElement = document.createElement("DIV");
+    newMacElement.classList.add(template['class']);
+    newId = template['id'] + num.toString();
+    newMacElement.setAttribute("id", "newId");
+    newMacElement.style.touchAction = template['touch-action'];
+    newMacElement.style.userSelect = template['user-select'];
+    newMacElement.style.margin = template['margin'];
+    newMacElement.style.backgroundColor = template['background-color'];
+    newMacElement.style.border = template['border'];
+    newMacElement.style.borderWidth: template['border-width'];
+    newMacElement.style.textAlign: template['text-align'];
+    newMacElement.style.clipPath: template['clip-path'];
+
+    //NOT CALLING RIGTH VALUES!! template['header]['header-class']';
+    let newMacHeader = document.createElement("DIV");
+    newMacHeader.classList.add(template['header']['header-class']);
+    newHeaderId = template['header']['header-id'] + num.toString();
+    newMacHeader.style.setAttribute("id", "newHeaderId");
+    newMacHeader.style.innerText = template['header']['header-text'] + num.toString();
+    newMacHeader.style.padding = template['header']['header-padding'];
+    newMacHeader.style.cursor = template['header']['header-cursor'];
+    newMacHeader.style.zIndex = template['header']['header-z-index'];
+    newMacHeader.style.backgroundColor = template['header']['header-background-color'];
+    newMacHeader.style.border = template['header']['header-border'];
+    newMacHeader.style.borderStyle = template['header']['header-border-style'];
+    newMacHeader.style.borderWidth = template['header']['header-border-width'];
+    newMacHeader.style.color = template['header']['header-color'];
+    newMacElement.appendChild(newMacHeader);
+
+    let newMacImage = document.createElement("IMG");
+    newMacImage.src = template['img'];
+    newMacElement.appendChild(newMacImage);
+
+    let newMacFooter = document.createElement("DIV");
+    newMacFooter.classList.add(template['footer']['footer-class']);
+    newFooterID = template['footer']['footer-id'] + num.toString();
+    newMacFooter.style.border = template['footer']['footer-border'];
+    newMacFooter.style.backgroundColor = template['footer']['footer-background-color'];
+    newMacFooter.style.zIndex = template['footer']['footer-z-index'];
+    newMacFooter.style.padding = template['footer']['footer-padding'];
+    newMacFooter.style.borderStyle = template['footer']['footer-border-style'];
+    newMacFooter.style.borderTopWidth = template['footer']['footer-'border-top-width];
+    newMacFooter.style.color = template['footer']['footer-color'];
+    newMacElement.appendChild(newMacFooter);
+
+    container.appendChild(newMacElement);
+
+  }
+}
+*/
+
+function print(){
+  console.log("HELLLLLLLLLLLO)");
 }
 
 function dragStart(e) {
@@ -155,7 +240,15 @@ function setTranslate(xPos, yPos, el) {
   el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
 }
 
-
+/*
+Function checks if items with class "draggableItem" are overlapping.
+dragItems element is all items that have the class "draggableItem"
+Function iterates over dragItems elements, checking two by two
+if items are overlapping, and sets colors occordingly.
+If two items overlap, and they are supposed to, they get set with green borders
+if they aren't supposed to, they get set with red borders.
+Items that have no overlap have black borders.
+*/
 function checkOverlap(el) {
   var flag_items = {
     "itemA": "False",
@@ -179,21 +272,22 @@ function checkOverlap(el) {
         rect1.bottom < rect2.top ||
         rect1.top > rect2.bottom)
       /* If item and el are not overlapping, set them to borderColor = black */
-      // if (!overlap) {
-      //   console.log("Item: ", item.id);
-      //   console.log("El: ", el.id);
-      //   console.log("NOT OVERLAPPING");
-      //   item.style.borderColor = "#000000";
-      //   el.style.borderColor = "#000000";
-      //   /* BUGGGGGGGG:
-      //   As loop goes on, it detects if pairs are not overlapping.
-      //   So A overlap with B
-      //   Then B not overlap with C
-      //   Then C not overlap with D
-      //   Because it goes in order from A to D, even though a and b overlap,
-      //   because B and C dont overlap, it sets both to black, changing what the first iteration did.
-      //   */
-      // }
+      /*
+      if (!overlap) {
+        console.log("Item: ", item.id);
+        console.log("El: ", el.id);
+        console.log("NOT OVERLAPPING");
+        item.style.borderColor = "#000000";
+        el.style.borderColor = "#000000";
+        /* BUGGGGGGGG:
+        As loop goes on, it detects if pairs are not overlapping.
+        So A overlap with B
+        Then B not overlap with C
+        Then C not overlap with D
+        Because it goes in order from A to D, even though a and b overlap,
+        because B and C dont overlap, it sets both to black, changing what the first iteration did.
+      }
+      */
       /* PROF NOTE: Here we confirm the overlap and can fire off specific behavior based
                   on the matching IDs */
       if (overlap) {
